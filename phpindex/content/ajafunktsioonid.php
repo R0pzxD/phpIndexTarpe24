22a1f067b894f8c09a1c705bcf630f5f18e0cc53
Opilane TTHK <opilane@tthk.ee>
2025-11-19 16:46:51 +0200

    date/time funktsioonide kasutamine

--- phpindex/content/ajafunktsioonid.php
+++ phpindex/content/ajafunktsioonid.php
@@ -1,2 +1,25 @@
 <?php
 echo "<h1>Ajafunktsioonid</h1>";
+echo "Tänane kuupäev".date("d.m.Y");
+date_default_timezone_set("Europe/Tallinn");
+echo "<br>";
+echo "<a href='https://www.php.net/manual/en/timezones.europe.php'>timeszones</a>";
+echo "<br>";
+echo "time()- aeg sekundites".time();
+echo "<br>";
+echo "date() -".date("d.m.Y G:i:s", time());
+echo "<pre>
+date('d.m.Y G:i:s', time());
+d - ? 01...34
+m - 1...12
+Y - neljakohane arv
+G - 24h formaat
+i - minutid 0..59
+s - sekundit 0...59
+</pre>";
+echo "<br>";
+echo "+1min = time()+60 ".date('d.m.Y G:i:s', time()+60);
+echo "<br>";
+echo "+1tund = time()+60 ".date('d.m.Y G:i:s', time()+60*60);
+echo "<br>";
+echo "+1päev = time()+60 ".date('d.m.Y G:i:s', time()+60*60*24);
